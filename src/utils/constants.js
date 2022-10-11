5e8c8d88c597f2c011e4a67084a2fc7cad6c805b
Hernan Tain <hernantain@gmail.com>
2022-10-11 02:45:58 -0300

    hotfix for constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,7 @@ const SPECIALTIES = [
 ]
 
 // IMPORTANT: This values should match with @appointments/utils/constants.js file
-const FREQUENCIES = [15, 30, 45, 60];
+const FREQUENCIES = [15, 30, 60];
 
 const MONTH_LABELS = {
   0: 'Enero',
