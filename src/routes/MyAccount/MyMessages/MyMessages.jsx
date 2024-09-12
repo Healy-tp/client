59840ba3a135dfe9db31442cd44db41a4ad2a5b8
Tomas <szemzotomas@gmail.com>
2024-09-11 21:12:55 -0300

    feat: add loader for messages

--- src/routes/MyAccount/MyMessages/MyMessages.jsx
+++ src/routes/MyAccount/MyMessages/MyMessages.jsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Container, CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from 'react-i18next';
 import { getConversations } from "../../../services/notifications";
@@ -8,14 +8,18 @@ import WelcomePage from "../WelcomePage";
 const MyMessages = ({ isDoctor, markMsgsReadCallback }) => {
   const [t] = useTranslation();
   const [conversations, setConversations] = useState([]);
+  const [isLoadingConversations, setIsloadingConversations] = useState(false);
 
   useEffect(() => {
     const getConversationsApi = async () => {
       try {
+        setIsloadingConversations(true);
         const response = await getConversations(isDoctor);
         setConversations(response);
       } catch (err) {
         console.log("could not get conversations from API", err);
+      } finally {
+        setIsloadingConversations(false);
       }
     };
     getConversationsApi();
@@ -23,17 +27,24 @@ const MyMessages = ({ isDoctor, markMsgsReadCallback }) => {
 
   return (
     <Container sx={{ padding: 4, display: "flex", flexDirection: "column" }}>
-      {conversations.length > 0 ? (
-        conversations.map((conv) => (
-          <Conversation
-            key={conv.id}
-            convData={conv}
-            isDoctor={isDoctor}
-            markMsgsReadCallback={markMsgsReadCallback}
-          />
-        ))
+      {isLoadingConversations ? (
+        <CircularProgress
+          size={60}
+          sx={{ position: "absolute", top: "40%", left: "50%" }}
+        /> 
       ) : (
-        <WelcomePage icon="inbox" title={t('my_account.my_messages.no_messages_title')} />
+        conversations.length > 0 ? (
+          conversations.map((conv) => (
+            <Conversation
+              key={conv.id}
+              convData={conv}
+              isDoctor={isDoctor}
+              markMsgsReadCallback={markMsgsReadCallback}
+            />
+          ))
+        ) : (
+          <WelcomePage icon="inbox" title={t('my_account.my_messages.no_messages_title')} />
+        )
       )}
     </Container>
   );
